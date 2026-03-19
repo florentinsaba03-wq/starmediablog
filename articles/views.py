27dc62aa050ddfa3d8be0d0894ca2f8d419de0e0
Florentinho <florentin.saba03@gmail.com>
2026-03-19 16:07:03 +0100

    fix: import Q in views.py, search page with suggestions

--- articles/views.py
+++ articles/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 from django.http import JsonResponse, FileResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.db.models import F
+from django.db.models import F, Q
 from django.db import IntegrityError
 
 
